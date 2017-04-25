bd933cff693b67ae6d77f6b7c0917e4f77bf42c8
Thierry Fleury <thierry@codinlab.fr>
2017-04-25 16:13:56 +0200

    Filtered

--- app/apiClient.ts
+++ app/apiClient.ts
@@ -3,14 +3,49 @@ import { IStatPrenom, Sexe } from "./statPrenom";
 
 export interface ApiResult<TData> {
     version: string;
-    bn_resultats: number;
+    nb_resultats: number;
     data: TData[];
+}
+
+export interface IApiFilter {
+    sexe?: Sexe;
+    prenom?: string;
+    annee?: number;
 }
 
 export class ApiClient {
     constructor(private baseUrl: string) {}
 
-    public list(): JQueryPromise<ApiResult<IStatPrenom>> {
-        return $.getJSON(this.baseUrl + "/content.json");
+    public list(filter?: IApiFilter): JQueryPromise<ApiResult<IStatPrenom>> {
+        if(filter) {
+            return $.getJSON(this.baseUrl + "/content.json").pipe(ApiClient.bindSexe).pipe(results => ApiClient.filter(results, filter));
+        } else {
+            return $.getJSON(this.baseUrl + "/content.json").pipe(ApiClient.bindSexe);
+        }
+    }
+
+    private static bindSexe(source: ApiResult<any>): ApiResult<IStatPrenom> {
+        source.data = source.data.map(stat => {
+            stat.sexe = Sexe[<string>stat.sexe];
+            return stat;
+        });
+        return source;
+    }
+
+    private static filter(source: ApiResult<IStatPrenom>, filter: IApiFilter): ApiResult<IStatPrenom> {
+        source.data = source.data.filter((entry) => {
+            if(filter.sexe && entry.sexe !== filter.sexe) {
+                return false;
+            }
+            if(filter.prenom && entry.prenom.toLowerCase().indexOf(filter.prenom.toLowerCase()) < 0) {
+                return false;
+            }
+            if(filter.annee && entry.annee_naissance !== filter.annee) {
+                return false;
+            }
+            return true;
+        });
+        source.nb_resultats = source.data.length;
+        return source;
     }
 }
--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,12 @@
-import { ApiClient, ApiResult } from "./apiClient"
+import { ApiClient, ApiResult, IApiFilter } from "./apiClient"
 import { IStatPrenom, Sexe } from "./statPrenom";
 
 class MyApp {
     private apiClient = new ApiClient("data");
 
     public bootstrap() {
+        $("#resultsTable > thead").on("change", "select, input", () => this.updateFilters())
+
         this.apiClient.list()
             .then((result) => {
                 this.showResults(result);
@@ -13,14 +15,35 @@ class MyApp {
 
     private showResults(results: ApiResult<IStatPrenom>) {
         let table = $("#resultsTable > tbody").empty();
+
         results.data.forEach((result) => {
             let row = $("<tr></tr>");
-            row.append(`<td>${result.sexe}</td>`);
+            row.append(`<td>${result.sexe === Sexe.FILLE ? "Fille" : "Garçon"}</td>`);
             row.append(`<td>${result.prenom}</td>`);
             row.append(`<td>${result.occurrence}</td>`);
             row.append(`<td>${result.annee_naissance}</td>`);
             table.append(row);
         });
+    }
+
+    private updateFilters() {
+        let filter: IApiFilter = {};
+        let sexe: string = $("#filterSexe").val();
+        if(sexe) {
+            filter.sexe = Sexe[sexe];
+        }
+
+        filter.prenom = $("#filterPrenom").val();
+
+        let annee = parseInt($("#filterAnnee").val());
+        if(!isNaN(annee)) {
+            filter.annee = annee;
+        }
+
+        this.apiClient.list(filter)
+            .then((result) => {
+                this.showResults(result);
+            });
     }
 }
 
