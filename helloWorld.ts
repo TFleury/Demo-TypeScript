c78c6adc00377577408648adb2ddfcbe037f733f
Thierry Fleury <thierry@codinlab.fr>
2017-04-25 12:12:05 +0200

    Hello world !

---
+++ helloWorld.ts
@@ -0,0 +1,8 @@
+function SayHello() {
+    let pageTitle = document.createElement("h1");
+    pageTitle.textContent = "Hello Code2be !";
+
+    document.body.appendChild(pageTitle);
+}
+
+SayHello();
